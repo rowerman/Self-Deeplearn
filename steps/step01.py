1f4dcd24a00d9ddd2c1ffe9bfe6e4d2e5831f166
rowerman <rowerman@sjtu.edu.cn>
2025-09-28 15:37:06 +0800

    three steps

---
+++ steps/step01.py
@@ -0,0 +1,9 @@
+class Variable:
+    def __init__(self, data):
+        self.data = data
+
+import numpy as np
+
+data = np.array(1.0)
+x = Variable(data)
+print(x.data)  # Should output: 1.0
---
+++ steps/step02.py
@@ -0,0 +1,13 @@
+from steps.step01 import Variable
+
+# 基类，后续所有函数类都在其基础上继承
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+    
+    # 抛出异常，告诉使用Function方法的人此方法应该通过继承来实现
+    def forward(self, x):
+        raise NotImplementedError()
